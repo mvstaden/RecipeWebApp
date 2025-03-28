609418a4e0883ae8bcd6d1662f82ca7c0a1ef4a9
Marius Van Staden <mvstaden91@gmail.com>
2025-03-28 14:54:51 +0200

    Added ingredients section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import bgFood from "./assets/images/image-omelette.jpeg";
 const App = () => {
   return (
-    <div className="h-screen  bg-Stone-100 flex items-center justify-center py-32 px-12">
+    <div className="bg-Stone-100 flex items-center justify-center py-32 px-12">
       <div className="bg-white w-[736px] p-10 rounded-3xl">
         <img src={bgFood} alt="" className="w-full rounded-xl" />
 
@@ -19,20 +19,36 @@ const App = () => {
 
           {/* Prepaparation Section */}
 
-          <section className="p-6 bg-Rose-50">
+          <section className="p-6 bg-Rose-50 rounded-xl ">
             <h3 className="font-semibold text-xl mb-4 text-Rose-800">
               Preparation Time
             </h3>
             <ul className="list-disc pl-6">
               <li className="indent-4 mb-2 text-Stone-600">
-                <span className="font-bold">Total</span>:Approximately 10
+                <span className="font-bold mr-1">Total:</span>Approximately 10
                 minutes
               </li>
-              <li className="indent-4 mb-2">
-                <span>Preparation:</span>5 minutes
+              <li className="indent-4 mb-2 text-Stone-600">
+                <span className="font-bold  mr-1">Preparation:</span>5 minutes
               </li>
-              <li className="indent-4 mb-2">
-                <span>Cooking:</span> 5 minutes
+              <li className="indent-4 mb-2 text-Stone-600">
+                <span className="font-bold mr-1">Cooking:</span> 5 minutes
+              </li>
+            </ul>
+          </section>
+
+          {/* Ingredients */}
+          <section>
+            <h3 className="text-Brown-800 text-[28px] font-Young mb-6">
+              Ingedrients
+            </h3>
+            <ul className="list-disc pl-5">
+              <li className="indent-4 mb-2">2-3 large eggs</li>
+              <li>Salt, to taste</li>
+              <li>Pepepr, to taste</li>
+              <li>1 tablespoon of butter or oil</li>
+              <li>
+                Optional fillings: cheese, diced vegetables, cooked meats, herbs
               </li>
             </ul>
           </section>
