ad3dae53bbc21f1264905d72b2593670532ab71c
Marius Van Staden <mvstaden91@gmail.com>
2025-04-02 04:01:06 +0200

    Made layout responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import bgFood from "./assets/images/image-omelette.jpeg";
 const App = () => {
   return (
-    <div className="bg-Stone-100 flex items-center justify-center py-32 px-12">
-      <div className="bg-white w-[736px] p-10 rounded-3xl">
-        <img src={bgFood} alt="" className="w-full rounded-xl" />
+    <div className="bg-Stone-100 flex items-center justify-center md:py-32 md:px-12">
+      <div className="bg-white w-[736px] md:p-10 md:rounded-3xl">
+        <img src={bgFood} alt="" className="w-full md:rounded-xl" />
 
-        <div className="flex flex-col gap-8 mt-10">
+        <div className="flex flex-col gap-8 md:mt-10 px-8 py-10 md:px-0 md:py-0">
           <section>
-            <h2 className="font-Young text-[40px] mb-6 text-Stone-900">
+            <h2 className="font-Young text-4xl md:text-[48px] mb-6 text-Stone-900">
               Simple Omelette Recipe
             </h2>
             <p className="font-Outfit text-Stone-600">
@@ -60,7 +60,7 @@ const App = () => {
             <h3 className="text-[28px] font-Young mb-6 text-Brown-800">
               Instructions
             </h3>
-            <ol className="list-decimal marker:text-Brown-800 text-Stone-600 pl-6 text-start ">
+            <ol className="list-decimal marker:text-Brown-800 text-Stone-600 pl-6 text-start mb-10">
               <li className="pl-4 mb-2 ">
                 <span className="font-bold ">Beat the eggs:</span>In a bowl,
                 beat the eggs with a pinch of salt and pepper until they are
@@ -95,10 +95,33 @@ const App = () => {
             </ol>
           </section>
 
-          <section>
-            <h3 className="text-[28px] text-Brown-800 mb-6 font-Young">
+          <section className="flex flex-col gap-6">
+            <h3 className="text-[28px] text-Brown-800  font-Young">
               Nutrition
             </h3>
+            <p className="text-Stone-600">
+              The table below shows nutritional values per serving without the
+              additional fillings.
+            </p>
+
+            <table className="w-full">
+              <tr className="border-b border-Stone-150">
+                <td className="pl-8 py-3 text-Stone-600">Calories</td>
+                <td className="pr-8 py-3 text-Brown-800 font-bold">277kcal</td>
+              </tr>
+              <tr className="border-b border-Stone-150 ">
+                <td className="pl-8 py-3 text-Stone-600">Carbs</td>
+                <td className="pr-8 py-3 text-Brown-800 font-bold">0g</td>
+              </tr>
+              <tr className="border-b border-Stone-150">
+                <td className="pl-8 py-3 text-Stone-600">Protein</td>
+                <td className="pr-8 py-3 text-Brown-800 font-bold">20g</td>
+              </tr>
+              <tr className="border-b border-Stone-150">
+                <td className="pl-8 py-3 text-Stone-600">Fat</td>
+                <td className="pr-8 py-3 text-Brown-800 font-bold">22g</td>
+              </tr>
+            </table>
           </section>
         </div>
       </div>
